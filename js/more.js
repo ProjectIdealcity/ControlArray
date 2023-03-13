1ef90190ffb8962a244670ff44e4b93c6a45ee01
LEFT_Flamelight <101847133+LEFTFlamelight@users.noreply.github.com>
2023-03-13 18:45:58 +0800

    粗略的修改了下
    没有很多修改细节，以后再改。

---
+++ js/more.js
@@ -0,0 +1,15 @@
+
+$(function(){
+
+	$(".search-btn").on("click", function(){
+      $(".search_sec").addClass("active");
+      return false;
+    });
+    $(".close-search").on("click", function(){
+      $(".search_sec").removeClass("active");
+      return false;
+    });
+	
+	
+	
+})
